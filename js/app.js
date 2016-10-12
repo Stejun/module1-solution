54f090a1692fe71799c8d66f8a0a9e205929b041
Stejun <stejun@wp.pl>
2016-10-12 20:12:59 +0200

    tested

--- js/app.js
+++ js/app.js
@@ -10,16 +10,15 @@
             var arrayChecked = CheckEmpty(arrayValues);
             var arrayCheckedLength = arrayChecked.length;
             $scope.class = "green";
-            if(arrayCheckedLength > 4){
+            if(arrayCheckedLength > 3){
                 $scope.note = "Too much!";
             }else if(arrayCheckedLength > 0 && arrayCheckedLength <= 3){
                 $scope.note = "Enjoy!";
-            }else{
+            }else if(arrayCheckedLength === 0){
                 $scope.note = "Please enter data first";
                 $scope.class = "red";
             }
         }
-        //I do NOT consider and empty item, i.e., `, ,` as an item towards to the count
         function CheckEmpty(array){
             var temp =[];
             array.forEach(function(val){
